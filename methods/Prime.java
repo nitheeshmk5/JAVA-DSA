567103b4eb911b948410a8e91c3000c344a7bb79
nitheeshmk5 <22107041@srcac.ac.in>
2024-07-07 09:44:26 +0530

    add prime

---
+++ methods/Prime.java
@@ -0,0 +1,27 @@
+package in.nitheeshmk.methods;
+
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        boolean ans = isPrime(num);
+        System.out.println(ans);
+    }
+
+    static boolean isPrime(int n){
+        if (n <= 1){
+            return false;
+        }
+        int c = 2;
+        while (c * c <= n){
+            if (n % c == 0){
+                return false;
+            }
+            c++;
+        }
+        return c*c > n;
+    }
+
+}
