a917635d6149b29089501b3cbf835d0c249495fc
nitheeshmk5 <22107041@srcac.ac.in>
2024-07-28 08:42:54 +0530

    add bubble sort

---
+++ arrays/sorting/Bubble.java
@@ -0,0 +1,28 @@
+package in.nitheeshmk.arrays.sorting;
+
+import java.util.Arrays;
+
+public class Bubble {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        bubble_sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubble_sort(int[] arr){
+        boolean swapped;
+        for (int i = 0; i < arr.length ; i++) {
+            swapped = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+    }
+}
